e99a10af2f3678b954211d37946f00bce26e14d2
Fatvol <107616873+Fatvol@users.noreply.github.com>
2022-11-26 14:30:31 +0300

    Add files via upload

---
+++ renkler.js
@@ -0,0 +1,9 @@
+document.querySelector(".sub1").onmouseover= function() {
+    document.querySelector(".sub1").style.color= "blue";
+};
+document.querySelector(".sub1").onmouseout= function() {
+    document.querySelector(".sub1").style.color= "purple";
+};
+document.querySelector(".nav").onmausover= function() {
+    document.querySelector(".nav").style.color="black"
+};
